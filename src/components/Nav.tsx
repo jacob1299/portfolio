79313cb994d58bc70e3454dde28c35167a8fcfd8
Jacob Badolato <jacobb1299@gmail.com>
2021-08-29 16:02:23 -0500

    Add routes and Nav bar

---
+++ src/components/Nav.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { BrowserRouter as Router, Link } from "react-router-dom";
+
+interface NavProps {
+  links: ILink[];
+}
+
+export type ILink = {
+  title: string;
+  to: string;
+};
+
+export const Nav: React.FC<NavProps> = ({ links }) => {
+  return (
+    <Router>
+      <div className="bg-gray-900 w-full flex flex-col items-end">
+        <div className="flex flex-row w-1/4 text-2xl justify-around">
+          {links.map((link, index) => (
+            <div className="text-gray-200 hover:text-blue-300">
+              <Link to={links[index].to} key={`link-${index}`}>
+                {links[index].title}
+              </Link>
+            </div>
+          ))}
+        </div>
+      </div>
+    </Router>
+  );
+};
---
+++ src/pages/Bio/Bio.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+
+
+export const Bio: React.FC = () => {
+    return (
+        <div className="border-2 border-blue-400">
+            <div className="flex flex-row p-2">
+                <h2>About Me</h2>
+                <p>
+                    Ever since I was a child, I have had been fascinated by the weather. You're probably wondering why this matters
+                    since I am a software engineer.
+                </p>
+            </div>
+        </div>
+    )
+}
